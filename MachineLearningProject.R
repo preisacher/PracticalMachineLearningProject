a4ed0dc6754252762a924b4057562e1fc8288cca
Philippe Reisacher <preisacher@users.noreply.github.com>
2016-01-26 19:40:03 -0700

    Create MachineLearningProject.R

---
+++ MachineLearningProject.R
@@ -0,0 +1,74 @@
+# The data for this project come from this source: http://groupware.les.inf.puc-rio.br/har. 
+# If you use the document you create for this class for any purpose please cite them as they have been 
+# very generous in allowing their data to be used for this kind of assignment.
+
+# Human Activity Recognition
+
+# What you should submit
+# The goal of your project is to predict the manner in which they did the exercise. 
+# This is the "classe" variable in the training set. You may use any of the other variables 
+# to predict with. You should create a report describing how you built your model, how you 
+# used cross validation, what you think the expected out of sample error is, and why you made 
+# the choices you did. You will also use your prediction model to predict 20 different test cases.
+
+# Reproducibility
+# Due to security concerns with the exchange of R code, your code will not be run during the 
+# evaluation by your classmates. Please be sure that if they download the repo, they will be 
+# able to view the compiled HTML version of your analysis.
+
+library(caret)
+## Loading required package: lattice
+## Loading required package: ggplot2
+library(rpart)
+library(rpart.plot)
+library(RColorBrewer)
+# library(rattle)
+## Rattle: A free graphical interface for data mining with R.
+## Version 3.1.0 Copyright (c) 2006-2014 Togaware Pty Ltd.
+## Type 'rattle()' to shake, rattle, and roll your data.
+library(randomForest)
+## randomForest 4.6-10
+## Type rfNews() to see new features/changes/bug fixes.
+#Finally, load the same seed with the following line of code:
+  
+set.seed(99876)
+
+# 
+# Pull the Training data into the trainData variable in memory
+#
+trainData <- read.table("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
+                         header=TRUE, sep=",", na.strings=c("NA","#DIV/0!",""), dec=".", strip.white=TRUE)
+
+summary(trainData)
+
+#
+# Pull the Test data into the testData variable in memory
+#
+testData <- read.table("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
+                       header=TRUE, sep=",", na.strings=c("NA","#DIV/0!",""), dec=".", strip.white=TRUE)
+summary(testData)
+
+#
+# Now, let us partition the trainData into myTrain and myTest for analysis and formula fitting
+#
+myTrain <- createDataPartition(y=trainData$classe, p=0.60, list=FALSE)
+myTraining <- trainData[myTrain, ]
+myTesting <- trainData[-myTrain, ]
+dim(myTraining) 
+dim(myTesting)
+
+#
+# Now I want to cleanup the near zero variance
+# Get rid of the near zero variances as they will cause poor results in my final
+# prediction model.
+#
+myTrainingNZV <- nearZeroVar(myTraining, saveMetrics=TRUE)
+myTrainingCleanup <- myTraining[!myTrainingNZV$nzv]
+dim(myTrainingCleanup)
+
+#
+# Get rid of the first column.  X is a sequential number for each row so not really going to help 
+# come up with a predictive model
+#
+myTrainingCleanup <- myTrainingCleanup[c(-1)]
+
