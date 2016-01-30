5d093284070078c1fc2479c33fc2a77df01eb39f
Philippe Reisacher <preisacher@users.noreply.github.com>
2016-01-29 19:10:14 -0700

    Update MachineLearningProject.R

--- MachineLearningProject.R
+++ MachineLearningProject.R
@@ -4,19 +4,21 @@
 
 # Human Activity Recognition
 
-# What you should submit
-# The goal of your project is to predict the manner in which they did the exercise. 
-# This is the "classe" variable in the training set. You may use any of the other variables 
-# to predict with. You should create a report describing how you built your model, how you 
-# used cross validation, what you think the expected out of sample error is, and why you made 
-# the choices you did. You will also use your prediction model to predict 20 different test cases.
-
+# In this exercise I am going to predict the manner in which the people we have data for 
+# did the exercise they ended up doing.  The variable that indicates this exercise is 
+# called "classe" in the training set and that is what I will be predicting.
+# The data in this analysis all came from source: http://groupware.les.inf.puc-rio.br/har.
+# who have been very generous in allowing their data to be used for this kind of exercise.
+# I will be using use the other variables (other than "classe") to predict with. 
+# This is my report describing how I built my model, and how Iused cross validation, 
+# what I think the expected out of sample error is, and why I made the choices I did. 
+# I will also use my prediction model to predict 20 different test cases.
+#
 # Reproducibility
-# Due to security concerns with the exchange of R code, your code will not be run during the 
-# evaluation by your classmates. Please be sure that if they download the repo, they will be 
-# able to view the compiled HTML version of your analysis.
-# required package: lattice
-# required package: ggplot2
+# Due to security concerns with the exchange of R code, my code will not be run during the 
+# evaluation by you, my classmates. You will be be able to view the 
+# compiled HTML version of my analysis which I will publish under Rpubs and provide a link.
+#
 
 library(caret)
 library(rpart)
@@ -24,47 +26,120 @@ library(rpart.plot)
 library(RColorBrewer)
 library(randomForest)
 
-# Setting a seed number for Reproducibility
-set.seed(99876)
+set.seed(54321)
 
 # 
-# Pull the Training data into the trainData variable into a memory data frame
+# Pull the Training data into the trainData variable in memory
 #
 trainData <- read.table("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
-                         header=TRUE, sep=",", na.strings=c("NA","#DIV/0!",""), dec=".", strip.white=TRUE)
+                        header=TRUE, sep=",", na.strings=c("NA","#DIV/0!",""), dec=".", strip.white=TRUE)
 
-summary(trainData)
+dim(trainData)
 
 #
-# Pull the Test data into the testData variable into a memory dataframe
+# Pull the Test data into the testData variable in memory
 #
 testData <- read.table("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
                        header=TRUE, sep=",", na.strings=c("NA","#DIV/0!",""), dec=".", strip.white=TRUE)
-summary(testData)
+dim(testData)
 
-#
-# Now, let us partition the trainData into myTrain and myTest for analysis and formula fitting
-#
-myTrain <- createDataPartition(y=trainData$classe, p=0.60, list=FALSE)
-myTraining <- trainData[myTrain, ]
-myTesting <- trainData[-myTrain, ]
+
+# Cleaning the Data
+# Now I want to cleanup the near zero variance
+# Get rid of the near zero variances as they will cause poor results in my final
+# prediction model.
+
+trainingNZV <- nearZeroVar(trainData, saveMetrics=TRUE)
+trainingCleanup <- trainData[!trainingNZV$nzv]
+dim(trainingCleanup)
+
+# Get rid of the first column.  X is a sequential number for each row so not really going to help 
+# with predicting anything.
+
+trainingCleanup <- trainingCleanup[c(-1)]
+
+# Now, i want to get rid of variables with too many Null values. 
+# For Variables that have more than a 30% threshold of Null values; I’m going to leave them out:
+
+trainingFinal <- trainingCleanup
+for(a in 1:length(trainingCleanup)) {
+  if(sum(is.na(trainingCleanup[,a]))/nrow(trainingCleanup) >= .3 ) { 
+    for(b in 1:length(trainingFinal)) {
+      if(length(grep(names(trainingCleanup[a]), names(trainingFinal)[b]))==1) { 
+        trainingFinal <- trainingFinal[,-b] 
+      }   
+    } 
+  }
+}
+
+dim(trainingFinal)
+
+# Partition the data for model fitting
+# Now, I want to partition the trainData into a training set and a testing set
+# for analysis and formula fitting.
+
+myTrainingPartition <- createDataPartition(y=trainingFinal$classe, p=0.60, list=FALSE)
+myTraining <- trainingFinal[myTrainingPartition, ]
+myTesting <- trainingFinal[-myTrainingPartition, ]
 
 dim(myTraining) 
 dim(myTesting)
 
-#
-# Now I want to cleanup the near zero variance
-# Get rid of the near zero variances as they will cause poor results in my final
-# prediction model.
-#
-myTrainingNZV <- nearZeroVar(myTraining, saveMetrics=TRUE)
-myTrainingCleanup <- myTraining[!myTrainingNZV$nzv]
+# I will now take the columns from myTraining that I am going to use
+# in the model and only keep those columns from the testData that match.
 
-dim(myTrainingCleanup)
+clean <- colnames(myTraining[, -58]) 
+testData <- testData[clean]
 
-#
-# Get rid of the first column.  X is a sequential number for each row so not really going to help 
-# come up with a predictive model
-#
-myTrainingCleanup <- myTrainingCleanup[c(-1)]
+# Making sure to sync with the same class between myTraining and the testData
 
+testData <- rbind(myTraining[2, -58] , testData) 
+testData <- testData[-1,]
+
+# Finding the best fit prediction model
+# Trying the ML algorithms for prediction: Decision Tree
+
+fit.dt <- rpart(classe ~ ., data=myTraining, method="class")
+
+# Predicting in-sample error
+
+predictions.dt <- predict(fit.dt, myTesting, type = "class")
+
+# Here is the confusionMatrix to see the results of Decision Tree
+
+confusionMatrix(predictions.dt, myTesting$classe)
+
+# Using ML algorithms for prediction: Random Forests
+
+fit.rf <- randomForest(classe ~. , data=myTraining)
+
+# Predicting in-sample error
+
+predictions.rf <- predict(fit.rf, myTesting, type = "class")
+
+# Here is the confusionMatrix to see the results of Random Forest
+
+confusionMatrix(predictions.rf, myTesting$classe)
+
+plot(fit.rf)
+
+# Plot showing the Mean Decrease Gini values indicating those predictors that are stronger 
+# (with the lower values)
+
+varImpPlot(fit.rf)
+
+# Choice of best fit is:
+# Random Forests is a better fit for predicting in this case
+# Here is the prediction 
+# Random Forests gave an Accuracy in the myTesting dataset of 99.87% which 
+# was more accurate that from the Decision Tree. 
+#
+# The expected out-of-sample error is 100-99.87 = 0.13%.
+#
+# Since the sample error is low, this suggests to me that my prediction model is accurate
+# and therefore, a good fit.
+
+# Predictions of test data
+
+predictions.rf.test <- predict(fit.rf, testData, type = "class")
+predictions.rf.test
